317679f1d06b3eea30fe28de5eb3545fdf07939a
paulodue <pauloedu98@hotmail.com>
2023-03-08 10:53:25 -0500

    assignment 8

--- COMP1030/Assigment8/src/CollegeCourseTestHarness.java
+++ COMP1030/Assigment8/src/CollegeCourseTestHarness.java
@@ -2,16 +2,21 @@ import java.util.Arrays;
 
 public class CollegeCourseTestHarness {
     public static void main(String[] args) {
-        int rows = 200;
-        int columns = 10;
+        int rows = 20;
+        int columns = 5;
 
         int[] courseArray = new int[rows];
-        Arrays.fill(courseArray, 0, 100, 0 );
-        Arrays.fill(courseArray, 101, 200, 9999);
+        Arrays.fill(courseArray, 0, 10, 0 );
+        Arrays.fill(courseArray, 11, 20, 9999);
 
         for (int ints : courseArray) {
             System.out.print(ints + " ");
         }
+
+        String newLine = System.getProperty("line.separator");
+        System.out.println(newLine + courseArray.length);
+
+
 
     }
 }
